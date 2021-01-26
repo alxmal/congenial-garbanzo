7e546bbc2d01311740fefa3a4f3b6f848d78d02d
Alexandr Malafeev <alxmalafeev@gmail.com>
2021-01-26 21:37:35 +0300

    update action

--- src/store/state.js
+++
@@ -1,8 +0,0 @@
-export default {
-    currentComics: {},
-    comicsNumRange: [1, 1],
-    isNumRangeUpdated: false,
-    isLoading: true,
-    origin: location.origin,
-    path: location.pathname
-};
--- src/utils/randomize.js
+++
@@ -1,9 +0,0 @@
-const randomize = range => {
-    const getRandom = rangeArr => {
-        let [min, max] = rangeArr;
-        return Math.round(Math.random() * (max - min) + min);
-    };
-    return getRandom(range);
-};
-
-export default randomize;
